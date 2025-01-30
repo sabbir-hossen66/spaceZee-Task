a3c1d993051cd2c98e7cfd4794dbcd447d5540ce
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-01-31 02:17:18 +0600

    categories headline create

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Title } from '../../commonComponent/Title';
+import { DynamicButton } from '../../commonComponent/DynamicButton';
+
 
 
 const Categories = () => {
   return (
-    <div>
-      <h2>here is categories</h2>
-      <Title heading='Categories' subheading='Choose your favorite category' align='left' />
+    <div className='container mx-auto flex-cols-1 lg:flex justify-between items-center my-15'>
+      <Title heading={'Categories'} />
+      <DynamicButton text={'View All Categories'} bgColor='bg-[#E7FAFE]' textColor='text-gray-900' />
+
     </div>
   );
 };
