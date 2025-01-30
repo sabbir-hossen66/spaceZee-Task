c57d55f9eb284c040fef4fb412d81a7f8ef961f0
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-01-31 03:07:14 +0600

    category section create

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { Title } from '../../commonComponent/Title';
 import { DynamicButton } from '../../commonComponent/DynamicButton';
+import { categories } from '../../data';
 
 
 
 const Categories = () => {
   return (
-    <div className='container mx-auto flex-cols-1 lg:flex justify-between items-center my-15'>
-      <Title heading={'Categories'} />
-      <DynamicButton text={'View All Categories'} bgColor='bg-[#E7FAFE]' textColor='text-gray-900' />
+    <div className='container mx-auto'>
+      <div className=' flex-cols-1 lg:flex justify-between items-center my-15'>
+        <Title heading={'Categories'} />
+        <DynamicButton text={'View All Categories'} bgColor='bg-[#E7FAFE]' textColor='text-gray-900' />
+      </div>
+
+      <div className="flex items-center justify-center p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
+          {categories.map((category, index) => (
+            <div key={index} className="px-6 text-center">
+              {/* Background Color Container */}
+              <div className={`${category.bgColor} rounded-lg p-6 relative`}>
+                {/* Category Name on Top of Background Color */}
+
+                {/* Image Below the Text */}
+                <div className="w-full h-32 overflow-hidden rounded-lg">
+                  <img src={category.image} alt={category.name} className="w-full h-full object-cover" />
+                </div>
+                <h2 className="text-xl font-semibold mt-4">{category.name}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
 
     </div>
   );
---
+++ src/data.jsx
@@ -0,0 +1,8 @@
+export const categories = [
+  { name: 'Breakfast', image: 'https://i.ibb.co.com/pgHjRwQ/category1.png', bgColor: 'bg-[#6CC63F00]' },
+  { name: 'Vegan', image: 'https://i.ibb.co.com/6R25cjny/category2.png', bgColor: '[#6CC63F00]' },
+  { name: 'Meat', image: 'https://i.ibb.co.com/9k9bPsMJ/category3.png', bgColor: 'bg-[#CC261B1A]' },
+  { name: 'Dessert', image: 'https://i.ibb.co.com/tMVXKgTx/category5.png', bgColor: 'bg-[#F09E001A] ' },
+  { name: 'Lunch', image: 'https://i.ibb.co.com/Zpj0HHYc/image-22.png', bgColor: 'white' },
+  { name: 'Chocolate', image: 'https://i.ibb.co.com/B5jZf2RM/image-23.png', bgColor: 'bg-[#F09E001A]' },
+];
