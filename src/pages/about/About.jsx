58ed0046be9137582898b93f3e80424bde876d7a
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-02-01 00:37:57 +0600

    about page responsive

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -6,8 +6,8 @@ import Client from '../../utility/Client';
 
 const About = () => {
   return (
-    <div className='my-20 max-w-screen-xl mx-auto px-6 py-12'>
-      <div className='text-center my-10'>
+    <div className=' max-w-screen-xl mx-auto px-6 py-12'>
+      <div className='text-center '>
         <Title heading={"About Us"} subheading={<>Welcome to our company! We are dedicated to making your life easier by providing <br />
           innovative and efficient solutions.  Our team of experts is committed  to delivering
           exceptional <br /> service, and we constantly strive to improve and evolve with the times</>} />
